7b2bb724bf19cafc5019344d63e08616415ab06c
stevenprichard <steven.prichard@boldcommerce.com>
2017-11-08 17:51:43 -0600

    init commit

---
+++ Sources/Application/Application.swift
@@ -0,0 +1,38 @@
+import Foundation
+import Kitura
+import LoggerAPI
+import Configuration
+import CloudEnvironment
+import KituraContracts
+import Health
+
+public let projectPath = ConfigurationManager.BasePath.project.path
+public let health = Health()
+
+public class App {
+    let router = Router()
+    let cloudEnv = CloudEnv()
+
+    public init() throws {
+    }
+
+    func postInit() throws {
+        // Capabilities
+        initializeMetrics(app: self)
+
+        // Endpoints
+        initializeHealthRoutes(app: self)
+        
+        router.get("/"){
+            request, response, next in
+            response.send("Hello Kitura")
+            next()
+        }
+    }
+
+    public func run() throws {
+        try postInit()
+        Kitura.addHTTPServer(onPort: cloudEnv.port, with: router)
+        Kitura.run()
+    }
+}
