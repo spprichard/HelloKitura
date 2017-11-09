6207a0b332b5d33f577a1321fc82be9cbb90f831
stevenprichard <steven.prichard@boldcommerce.com>
2017-11-08 21:56:20 -0600

    added words

--- Sources/Application/NameHandler.swift
+++ Sources/Application/NameHandler.swift
@@ -4,5 +4,5 @@ import Kitura
 func sayHelloHandler(request: RouterRequest, response: RouterResponse, next: ()->Void) -> Void {
     Log.info("sayHelloHandler")
     let name = request.parameters["name"] ?? "IDK your name..."
-    response.send("Hello \(name.capitalized)")
+    response.send("Hello \(name.capitalized)...welcome")
 }
